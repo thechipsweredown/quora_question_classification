2cf0fe73835c618cc73da95e1d27484c08390222
tungdd <tungdangduc@admicro.vn>
2019-03-13 17:21:51 +0700

    fix path

--- train_svm.py
+++ train_svm.py
@@ -22,27 +22,27 @@ def train():
     train_data = vectorizer.transform(train_data)
     clf = LinearSVC(verbose=True)
     clf.fit(train_data, train_labels)
-    joblib.dump(vectorizer, "../model/vectorzier.intend.pkl")
-    joblib.dump(clf, "../model/classify.pkl")
+    joblib.dump(vectorizer, "model/vectorzier.intend.pkl")
+    joblib.dump(clf, "model/classify.pkl")
 
 
 def svc_param_selection(kfolds):
     cs = [0.001, 0.01, 0.1, 1, 10]
     gammas = [0.001, 0.01, 0.1, 1]
     train_data, train_labels = load_data_train()
-    vectorizer = joblib.load("../model/vectorzier_quora.pkl")
+    vectorizer = joblib.load("model/vectorzier_quora.pkl")
     train_data = vectorizer.transform(train_data)
     param_grid = {'C': cs, 'gamma': gammas}
     clf = GridSearchCV(SVC(kernel='rbf'), param_grid, cv=kfolds, verbose=True)
     print("---------------START---------------")
     clf.fit(train_data, train_labels)
-    joblib.dump(clf, "../model/grid_quora.pkl")
+    joblib.dump(clf, "model/grid_quora.pkl")
 
 
 def test():
-    vectorizer = joblib.load("../model/vectorzier_quora.pkl")
-    clf = joblib.load("../model/grid_quora.pkl")
-    with open('../result/result.csv', 'w') as f1:
+    vectorizer = joblib.load("model/vectorzier_quora.pkl")
+    clf = joblib.load("model/grid_quora.pkl")
+    with open('result/result.csv', 'w') as f1:
         f1.write("qid,prediction\n")
         df_test = pd.read_csv('data/test.csv')
         for index, row in df_test.iterrows():
